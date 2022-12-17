c62d7e285ca8437589cba1ba80c81e61b1927d8a
MelTRIPOT <tripot.melanie@gmail.com>
2022-12-17 23:16:20 +0100

    CandyShop

--- SmoothiesFarmUnityProject/Assets/SmoothiesFarm/CandyShop/SpawnManager.cs
+++ SmoothiesFarmUnityProject/Assets/SmoothiesFarm/CandyShop/SpawnManager.cs
@@ -5,28 +5,37 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject Bonbon;
-    [SerializeField] private GameObject[] SpawnPoint;
+    [SerializeField] private Bonbon Bonbon;
+    [SerializeField] private List<SpawnPoint> SpawnPoint;
+    private List<SpawnPoint> _SpawnPoint;
     private int BonbonNb;
 
     void Start()
     {
-        
+        // choose 3 spawn point
+        for (int i = 0; i < 3; i++)
+        {
+            SpawnNewBonbon();
+        }
     }
 
     public void GetAvailableSpawnPoint()
     {
-
+        _SpawnPoint= SpawnPoint.FindAll(s => s.IsAvailable);           
     }
 
     public void HandleBonbonCaught()
     {
-
+        SpawnNewBonbon();
     }
 
+    //fait spawnner un nouveau bonbon à l'emplacement d'un spwnpoint dispo puis rend le spawnpoint indisponible 
     public void SpawnNewBonbon()
     {
-
+        GetAvailableSpawnPoint();
+        int i = Random.Range(0, _SpawnPoint.Count - 1);
+        Instantiate(Bonbon, _SpawnPoint[i].transform);
+        SpawnPoint.Find(S => S = _SpawnPoint[i]).IsAvailable =false;
     }
 
 }
