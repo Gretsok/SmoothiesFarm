9d9d57b437ec98289deb5902fda9771ecb2d35d4
Fergal MECHIN <47755843+Gretsok@users.noreply.github.com>
2022-12-18 15:23:25 +0100

    More UI

---
+++ SmoothiesFarmUnityProject/Assets/SmoothiesFarm/RatAttack/UI/DeliveringInfoWidget.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace SmoothiesFarm.RatAttack
+{
+    public class DeliveringInfoWidget : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_durationWidget = 3f;
+
+        private float m_timeOfStart = 0;
+
+        private void OnEnable()
+        {
+            m_timeOfStart = Time.time;
+        }
+
+        private void Update()
+        {
+            if(Time.time - m_timeOfStart > m_durationWidget)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
